6e63e431056327baaf95a07dd366f2d504805796
ehsankader16 <1805067@ugrad.cse.buet.ac.bd>
2023-02-20 14:42:51 +0600

    postgres connected

--- creative_production_management/creative_production_management/settings.py
+++ creative_production_management/creative_production_management/settings.py
@@ -74,10 +74,31 @@ WSGI_APPLICATION = 'creative_production_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'OPTIONS': {
+    #         'NAME': 'bvieeszx',
+    #         'USER': 'bvieeszx',
+    #         'PASSWORD': '87sV977DrVkXDGLWhJP3ego37WeSZCHE',
+    #         'HOST': 'tiny.db.elephantsql.com',
+    #         'PORT': '5432'
+    #     },
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bvieeszx',
+        'USER': 'bvieeszx',
+        'PASSWORD': '87sV977DrVkXDGLWhJP3ego37WeSZCHE',
+        'HOST': 'tiny.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
