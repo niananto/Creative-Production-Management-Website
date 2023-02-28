ebe371e30658d2329925a393d54d65e6584b934c
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2023-03-01 01:04:50 +0600

    added vercel things

--- creative_production_management/creative_production_management/settings.py
+++ creative_production_management/creative_production_management/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-s$xmme(hgn7132jskbq64kp&x2^95566fpd*or-ugrxcwghx90
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -144,3 +144,8 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
