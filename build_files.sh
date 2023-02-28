c12ecf79d7f350b6f3250c49cf1fd64a01a27142
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2023-03-01 01:28:28 +0600

    new pip freeze and venv setup in build sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
+echo "SETTING VENV"
+python -m pip install --upgrade pip
+python -m pip install virtualenv
+python -m virtualenv venv
+source venv/bin/activate
+echo "SETTING VENV END"
+
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python -m pip install -r requirements.txt
+cd creative_production_management
+python manage.py collectstatic --noinput
 echo "BUILD END"
